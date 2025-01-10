506e75dde8afe4f4c8b43ec5c3592bdbcd5a56ad
EvertVanGroningen <145978161+EvertVanGroningen@users.noreply.github.com>
2025-01-10 12:15:19 +0100

    Langer

--- My project/Assets/Scripts/Movement/walk.cs
+++ My project/Assets/Scripts/Movement/walk.cs
@@ -6,16 +6,33 @@ public class walk : MonoBehaviour
     private float moveSpeedGround = 7f;
     private float moveSpeedAir = 4f;
     private Rigidbody2D rb;
+    State state;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
+
+    public enum State 
+    {
+        InAir,
+        OnGround,
+    }
+
     void Update()
     {
         float moveInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
 
         if(rb.velocity.y == 0)
+        {
+            state = State.OnGround;
+        }
+        else
+        {
+            state = State.InAir;
+        }
+        
+        if (state == State.OnGround)
         {
             moveSpeed = moveSpeedGround;
         }
